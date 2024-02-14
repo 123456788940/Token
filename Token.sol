4964a94dd646d1f30fec847e74ef189ac2cb131f
Soham Dutta <123827172+123456788940@users.noreply.github.com>
2024-02-14 11:25:37 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -12,12 +12,11 @@ contract MyContract {
         owner=_owner;
     }
 
-    function transferTokens(address _recipient, uint256 _value) external {
+    function transferTokens(address _recipient, uint256 _value) external payable{
         require(_recipient != address(0), "Invalid recipient address");
         require(_value > 0, "Invalid transfer amount");
 
-        // Call the transfer function of the TRC20 token contract
-        require(token.transfer(_recipient, _value), "Transfer failed");
+        require(token.transfer(_recipient, _value));
 
         // Emit an event indicating the transfer
         emit TokensTransferred(msg.sender, _recipient, _value);
@@ -28,8 +27,7 @@ contract MyContract {
         require(_to != address(0), "Invalid recipient address");
         require(_value > 0, "Invalid transfer amount");
 
-        // Call the transferFrom function of the TRC20 token contract
-        require(token.transferFrom(_from, _to, _value), "Transfer from failed");
+      require(token.transferFrom(_from, _to, _value));
 
         // Emit an event indicating the transfer
         emit TokensTransferred(_from, _to, _value);
@@ -38,30 +36,4 @@ contract MyContract {
     // Event emitted when tokens are transferred
     event TokensTransferred(address indexed _from, address indexed _to, uint256 _value);
 
-    
-    function totalSupply() external view returns (uint256) {
-        return token.totalSupply();
-    }
-
-     function balanceOf() external view returns (uint256) {
-        return token.balanceOf(owner);
-    }
-
-     function allowance(address _owner, address _spender) external view returns (uint256) {
-        return token.allowance(_owner, _spender);
-    }
-
-      function approve(address _spender, uint256 _value) external returns (bool) {
-        require(_spender != address(0), "Invalid spender address");
-
-        // Call the approve function of the TRC20 token contract
-        require(token.approve(_spender, _value), "Approval failed");
-
-        // Emit an event indicating the approval
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    // Event emitted when approval is granted
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
